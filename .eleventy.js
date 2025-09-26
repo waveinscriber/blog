29bdde1c2b8d0d4f5e124d76c6bbec5766f1a44f
waveinscriber <waveinscriber@gmail.com>
2025-09-26 04:40:06 +0300

    GitHub Pages path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 export default function (eleventyConfig) {
     return {
-        pathPrefix: process.env.PATH_PREFIX || '',
         dir: {
             input: 'src',
             includes: '_includes',
