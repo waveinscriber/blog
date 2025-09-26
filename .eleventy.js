3efeaf06e3d6b2b5834a7451f78d69fac65cf76e
waveinscriber <waveinscriber@gmail.com>
2025-09-26 04:12:03 +0300

    initialize with 11ty

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+export default function (eleventyConfig) {
+    return {
+        pathPrefix: process.env.PATH_PREFIX || '',
+        dir: {
+            input: 'src',
+            includes: '_includes',
+            output: '_site',
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+    };
+}
