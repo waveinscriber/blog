2ecbc0e6547fc4ec6107b9ed405a601a709c2e14
waveinscriber <waveinscriber@gmail.com>
2025-09-26 05:01:41 +0300

    fix GitHub Pages path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,18 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
 export default function (eleventyConfig) {
-    return {
-        dir: {
-            input: 'src',
-            includes: '_includes',
-            output: '_site',
-        },
-        templateFormats: ['md', 'njk', 'html'],
-        markdownTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-    };
+  eleventyConfig.addPlugin(HtmlBasePlugin);
 }
+
+export const config = {
+  pathPrefix: process.env.PATH_PREFIX || "",
+  dir: {
+    input: "src",
+    includes: "_includes",
+    output: "_site",
+  },
+  templateFormats: ["md", "njk", "html"],
+  markdownTemplateEngine: "njk",
+  htmlTemplateEngine: "njk",
+  dataTemplateEngine: "njk",
+};
