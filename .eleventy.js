c18034261af4db17ede345241bfe44ad79dd0221
waveinscriber <waveinscriber@gmail.com>
2025-10-16 11:30:34 +0300

    basic layouts and navbar

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,22 @@
 import { HtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(HtmlBasePlugin);
+    eleventyConfig.addPlugin(HtmlBasePlugin);
+
+    eleventyConfig.addPassthroughCopy("src/css/");
+
+    eleventyConfig.addWatchTarget("src/css/");
 }
 
 export const config = {
-  pathPrefix: process.env.PATH_PREFIX || "",
-  dir: {
-    input: "src",
-    includes: "_includes",
-    output: "_site",
-  },
-  templateFormats: ["md", "njk", "html"],
-  markdownTemplateEngine: "njk",
-  htmlTemplateEngine: "njk",
-  dataTemplateEngine: "njk",
+    pathPrefix: process.env.PATH_PREFIX || "",
+    dir: {
+        input: "src",
+        includes: "_includes",
+        output: "_site",
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
 };
